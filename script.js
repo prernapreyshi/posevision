49557bab3d7f6936efe1c55f014cc274198f81ce
Prerna Preyshi <prernapreyshi2105@gmail.com>
2025-12-27 22:45:00 +0530

    change camera captureing UI

--- script.js
+++ script.js
@@ -223,13 +223,3 @@ holistic.onResults((results) => {
   ctx.restore();
 });
 
-// ---------- CAMERA ----------
-const camera = new Camera(video, {
-  onFrame: async () => {
-    await holistic.send({ image: video });
-  },
-  width: isMobile ? 360 : 640,
-  height: 480,
-});
-
-camera.start();
